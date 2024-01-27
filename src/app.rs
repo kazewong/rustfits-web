6035c9e00014463146eb510c99050fb3f8cd9430
Kaze Wong <kazewong.physics@gmail.com>
2024-01-27 11:06:58 -0500

    Add dependencies and update navbar styling

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,144 @@
 use yew::prelude::*;
 use crate::navbar::Navbar;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <Navbar />
-        </main>
+use std::collections::HashMap;
+
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
+use gloo::file::callbacks::FileReader;
+use gloo::file::File;
+use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
+use yew::html::TargetCast;
+use yew::{html, Callback, Component, Context, Html};
+
+
+struct FileDetails {
+    name: String,
+    file_type: String,
+    data: Vec<u8>,
+}
+pub enum Msg {
+    Loaded(String, String, Vec<u8>),
+    Files(Vec<File>),
+}
+
+pub struct App {
+    readers: HashMap<String, FileReader>,
+    files: Vec<FileDetails>,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            readers: HashMap::default(),
+            files: Vec::default(),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Loaded(file_name, file_type, data) => {
+                self.files.push(FileDetails {
+                    data,
+                    file_type,
+                    name: file_name.clone(),
+                });
+                self.readers.remove(&file_name);
+                true
+            }
+            Msg::Files(files) => {
+                for file in files.into_iter() {
+                    let file_name = file.name();
+                    let file_type = file.raw_mime_type();
+
+                    let task = {
+                        let link = ctx.link().clone();
+                        let file_name = file_name.clone();
+
+                        gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                            link.send_message(Msg::Loaded(
+                                file_name,
+                                file_type,
+                                res.expect("failed to read file"),
+                            ))
+                        })
+                    };
+                    self.readers.insert(file_name, task);
+                }
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="wrapper">
+                <p id="title">{ "Upload Your Files To The Cloud" }</p>
+                <label for="file-upload">
+                    <div
+                        id="drop-container"
+                        ondrop={ctx.link().callback(|event: DragEvent| {
+                            event.prevent_default();
+                            let files = event.data_transfer().unwrap().files();
+                            Self::upload_files(files)
+                        })}
+                        ondragover={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                        ondragenter={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                    >
+                        <i class="fa fa-cloud-upload"></i>
+                        <p>{"Drop your fits here or click to select"}</p>
+                    </div>
+                </label>
+                <input
+                    id="file-upload"
+                    type="file"
+                    accept=".fits"
+                    multiple={false}
+                    onchange={ctx.link().callback(move |e: Event| {
+                        let input: HtmlInputElement = e.target_unchecked_into();
+                        Self::upload_files(input.files())
+                    })}
+                />
+                <div id="preview-area">
+                    { for self.files.iter().map(Self::view_file) }
+                </div>
+            </div>
+        }
+    }
+}
+
+impl App {
+    fn view_file(file: &FileDetails) -> Html {
+        html! {
+            <div class="preview-tile">
+                <p class="preview-name">{ format!("{}", file.name) }</p>
+                <div class="preview-media">
+                    if file.file_type.contains("fits"){
+
+                    }
+                </div>
+            </div>
+        }
+    }
+
+    fn upload_files(files: Option<FileList>) -> Msg {
+        let mut result = Vec::new();
+
+        if let Some(files) = files {
+            let files = js_sys::try_iter(&files)
+                .unwrap()
+                .unwrap()
+                .map(|v| web_sys::File::from(v.unwrap()))
+                .map(File::from);
+            result.extend(files);
+        }
+        Msg::Files(result)
     }
 }
--- src/navbar.rs
+++ src/navbar.rs
@@ -3,9 +3,12 @@ use yew::prelude::*;
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
     html! {
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button class="font-bold bg-red-500 rounded-md">
-                {"Home"}
+        <nav class="flex items-center">
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">
+                {"File"}
+            </button>
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">
+                {"File"}
             </button>
 
         </nav>
