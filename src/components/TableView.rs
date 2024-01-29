bb39320176b465b81a0214ca3dbbc54391527b19
Kaze Wong <kazewong.physics@gmail.com>
2024-01-29 13:12:23 -0500

    Add Matrix2D support to TableView component

--- src/components/TableView.rs
+++ src/components/TableView.rs
@@ -1,2 +1,33 @@
-use rustfits::data::tables::ASCIIField;
+use rustfits::data::tables::{ASCIIField, Matrix2D};
+use yew::{html, props, Component, Context, Html, Properties};
 
+pub struct TableView{
+}
+
+#[derive(Properties, PartialEq)]
+pub struct Props{
+    pub data: Matrix2D<ASCIIField>,
+}
+
+impl Component for TableView{
+
+    type Message = ();
+    type Properties = Props;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self{
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        todo!()
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html{
+
+        html!{
+            <>
+            </>
+        }
+    }
+}
