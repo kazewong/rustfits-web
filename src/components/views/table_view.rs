f84179b715a2f149417c73694e09855e04956f30
Kaze Wong <kazewong.physics@gmail.com>
2024-02-19 18:45:37 -0500

    Refactor TableView component and update dependencies

--- src/components/views/table_view.rs
+++ src/components/views/table_view.rs
@@ -1,75 +1,73 @@
-
 use core::fmt;
-
+use log::info;
 use rustfits::data::tables::{ASCIIField, BinaryField, Matrix2D};
 use yew::{html, Component, Context, Html, Properties};
-use log::info;
-pub struct TableView{
-}
+pub struct TableView {}
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Tables{
+pub enum Tables {
     ASCII(Matrix2D<ASCIIField>),
     Binary(Matrix2D<BinaryField>),
 }
 
-pub enum Fields{
+pub enum Fields {
     ASCII(ASCIIField),
     Binary(BinaryField),
 }
 
-impl fmt::Display for Fields{
+impl fmt::Display for Fields {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self{
+        match self {
             Fields::ASCII(field) => {
                 write!(f, "{}", field)
-            },
+            }
             Fields::Binary(field) => {
                 write!(f, "{}", field)
-            },
+            }
         }
     }
 }
 
-impl Tables{
-    pub fn get_row(&self, row: usize) -> Vec<Fields>{
-        match self{
-            Tables::ASCII(table) => {
-                table.get_row(row.try_into().unwrap()).iter().map(|x| Fields::ASCII(x.clone())).collect()
-            },
-            Tables::Binary(table) => {
-                table.get_row(row.try_into().unwrap()).iter().map(|x| Fields::Binary(x.clone())).collect()
-            },
+impl Tables {
+    pub fn get_row(&self, row: usize) -> Vec<Fields> {
+        match self {
+            Tables::ASCII(table) => table
+                .get_row(row.try_into().unwrap())
+                .iter()
+                .map(|x| Fields::ASCII(x.clone()))
+                .collect(),
+            Tables::Binary(table) => table
+                .get_row(row.try_into().unwrap())
+                .iter()
+                .map(|x| Fields::Binary(x.clone()))
+                .collect(),
         }
     }
 }
 
 #[derive(Properties, PartialEq)]
-pub struct Props{
+pub struct Props {
     pub data: Tables,
 }
 
-impl Component for TableView{
-
+impl Component for TableView {
     type Message = ();
     type Properties = Props;
 
     fn create(ctx: &Context<Self>) -> Self {
         info!("TableView created");
-        Self{
-        }
+        Self {}
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         todo!()
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html{
-
+    fn view(&self, ctx: &Context<Self>) -> Html {
         let data = ctx.props().data.clone();
         let row = data.get_row(0);
 
-        html!{
+        html! {
             <>
             {row.iter().collect::<Html>()}
             </>
